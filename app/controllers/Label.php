2ddaaa0ba824c5e945b858e20bb526353a57ae53
Shawn <PushShawn@gmail.com>
2018-06-26 12:09:20 +0800

    勉强运行

--- app/controllers/Label.php
+++ app/controllers/Label.php
@@ -21,11 +21,42 @@ class Label extends Account
         return $this->fetch('/label/index');
     }
 
+
+    /**
+     * 获取事件统计信息
+     *
+     * @param $_POST['belongDate'] 前端传来的所属日期
+     * @param $this->getUserId() 当前用户编号
+     *
+     * @return $this->msg['status'] 状态码
+     * @return $this->msg['info'] 提示信息
+     * @return $this->msg['url'] 跳转链接
+     * @return $this->msg['labelList'] 事件列表
+     */
+    public function acceptLabelList()
+    {
+        // 验证用户是否已登录
+        if( !$this->isLogin() ) {
+            $this->echoJsonMsg(400, USER_IS_NOT_LOGIN, '/account/login');
+        }
+
+        // 获取事件信息
+        $this->msg['labelList'] = $this->getLabelList();
+        // 判断事件列表是否为空
+        if( empty($this->msg['labelList']) ) {
+            $this->echoJsonMsg(400, EVENT_LIST_IS_NULL, '#');
+        } else {
+            // 成功获取事件列表
+            $this->echoJsonMsg(200, GET_EVENT_LIST_SUCCESS, '#');
+        }
+    }
+
+
     /**
      * 添加标签
      * @return [type] [description]
      */
-    public function sendLabel()
+    public function deleteLabel()
     {
         // 验证用户是否已登录
         if( !$this->isLogin() ) {
@@ -33,26 +64,55 @@ class Label extends Account
         }
 
         // 接收数据
-        if( !empty($_POST['tagName']) ) {
-            $this->tagName = $_POST['tagName'];
+        if( !empty($_POST['tagId']) ) {
+            $this->tagId = $_POST['tagId'];
         } else {
-            $this->echoJsonMsg(400, "标签名为空", '/label/add');
-        }
-
-        //接收数据
-         if( !empty($_POST['tagColor']) ) {
-            $this->tagColor = $_POST['tagColor'];
-        } else {
-            $this->echoJsonMsg(400, "标签颜色为空", '/label/add');
+            $this->echoJsonMsg(400, "标签编号为空", '#');
         }
 
         // 设置事项
-        if( $this->setTag() ) {
-            $this->echoJsonMsg(200, "添加标签成功", '#');
+        if( $this->deleteTag() ) {
+            $this->echoJsonMsg(200, "删除标签成功", '#');
         } else {
-            $this->echoJsonMsg(400, "添加标签失败", '#');
+            $this->echoJsonMsg(400, "删除标签失败", '#');
         }
     }
+
+
+    /**
+     * 设置标签数据库表
+     */
+    private function deleteTag() {
+        $count = (new Tag())->delete($this->tagId);
+
+        // 写入标签
+        if( $count == 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 组装事件
+     *
+     * @param $this->getUserId() 当前用户编号
+     * @param $this->startDate 开始日期
+     * @param $this->endData 结束日期
+     *
+     * @return $labelList 事件列表，包括：
+     * @return $labelList['tag_name'] 标签名
+     * @return $labelList['time_length'] 标签总计时长
+     * @return $labelList['date_length'] 时间跨度
+     * @return $labelList['time_length_per_day'] 每天标签时长
+     */
+    private function getLabelList() {
+        // 重新组装事件
+        // tag_id 和 时长（取决于记录数量）
+        $labelList = (new Tag())->selectAll();
+        return  $labelList;
+    }
+
 
     /**
      * 设置标签数据库表
--- static/js/label.js
+++ static/js/label.js
@@ -1,4 +1,135 @@
 /**
+ * 自动加载
+ */
+window.onload = function(){
+
+    // 获取事件
+    acceptLabelList();
+};
+
+/**
+ * 获取事件数据
+ */
+function acceptLabelList(){
+
+    // 获取开始日期，默认当前日期前一周
+    var data = {};
+
+    // 测试信息展示
+    $('#debug-info').append("<br><br> acceptLabelList() ");
+    $('#debug-info').append("<br> 发送数据：" + JSON.stringify(data) );
+
+    $.ajax({
+        type: "POST",
+        url: "/label/acceptLabelList",
+        data: data,  //提交到后台的数据
+        dataType: "json",   //回调函数接收数据的数据格式
+        success: function(msg){
+
+            // 测试信息展示
+            $('#debug-info').append("<br> 返回数据：" + JSON.stringify(msg) );
+
+            // 状态码，获取事件成功
+            if(msg.status == 200){
+
+                // DOM 展示事件列表
+                showLabelList_DOM(msg.labelList);
+
+                // bootstrap table  展示事件列表
+                // showEventList_bootstrap(eventList);
+
+            }else{
+                // 模态框显示状态码、提示信息、跳转链接
+                showInfoModal( msg.status, msg.info, msg.url);
+            }
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            // 模态框显示状态码、提示信息、跳转链接
+            showInfoModal( '404', 'ajax 请求错误！', '#');
+        },
+    });
+}
+
+
+/**
+ * DOM 展示列表
+ * @param  {[type]} objList [description]
+ * @return {[type]}         [description]
+ */
+function showLabelList_DOM(objList)
+{
+    // 测试信息展示
+    $('#debug-info').append("<br><br> showLabelList_DOM() ");
+    $('#debug-info').append("<br> 数据：" + JSON.stringify(objList) );
+
+    // 清空表格旧数据
+    $("#label-manage-list tr:not(:first)").html("");
+    // 获取表格 id
+    var table = document.getElementById("label-manage-list");
+
+    for(var i=0; i<objList.length; ++i)
+    {
+
+        var row = table.insertRow(-1);
+        var obj = objList[i];
+
+        // 标签对象
+        var tagId   = obj.id;
+        var tagName = obj.tag_name;
+        var ownerId = obj.owner_id;
+        var tagColor = obj.tag_color;
+
+        for(var key in obj){
+            var cell = row.insertCell(-1);
+            cell.innerHTML=obj[key];
+        }
+        var cellDelete=row.insertCell(-1);
+        // 插入按钮
+        cellDelete.innerHTML = '<button class="btn btn-block label-btn "' +
+        ' onclick="deleteLabel(' + tagId +')"' +
+        // 设置文本为白色，按钮颜色
+        ' style="background-color:'+ tagColor +';"' +
+        '>' + tagName + '</button>';
+    }
+}
+
+
+function deleteLabel(tagId){
+
+    var data = {};
+    data.tagId = tagId;
+
+    // 测试信息展示
+    $('#debug-info').append("<br><br> deleteLabel(tagId)");
+    $('#debug-info').append("<br> 发送数据 ：" + JSON.stringify(data) );
+
+    $.ajax({
+        type: "POST",
+        url:  "/label/deleteLabel",
+        data: data,  //提交到后台的数据
+        dataType: "json",   //回调函数接收数据的数据格式
+        success: function(msg){
+
+            // 测试信息展示
+            $('#debug-info').append("<br> 返回数据 ：" + JSON.stringify(msg) );
+
+            // 状态码
+            if(msg.status == 200){
+                // 添加成功,刷新当前页面
+                acceptBlockAndTag();
+            }else{
+                // 模态框显示状态码、提示信息、跳转链接
+                showInfoModal( msg.status, msg.info, msg.url);
+            }
+        },
+        error:  function(XMLHttpRequest, textStatus, errorThrown) {
+            // 模态框显示状态码、提示信息、跳转链接
+            showInfoModal( '404', 'ajax 请求错误！', '#');
+        },
+    });
+}
+
+/**
  * 添加标签
  * @return {[type]}         [description]
  */
